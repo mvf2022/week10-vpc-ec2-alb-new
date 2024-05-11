32664208390c489cc6baa1d7142c0e1bc38089f1
mvf2022 <116031022+mvf2022@users.noreply.github.com>
2024-05-10 19:53:21 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "week10-s3"
+    bucket         = "week10-bucket1"
     key            = "test/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
