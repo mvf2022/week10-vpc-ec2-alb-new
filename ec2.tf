e8fce3af0fae470090840f54d5c5541d1a5ca556
France Valeririe Madjoukouo <js8francemadj@gmail.com>
2024-05-11 12:16:28 -0500

    changes

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "server1" {
   ami = "ami-02d7fd1c2af6eead0"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
   availability_zone = "us-east-1a"
   subnet_id = aws_subnet.private1.id
@@ -12,7 +12,7 @@ resource "aws_instance" "server1" {
 }
 resource "aws_instance" "server2" {
   ami = "ami-02d7fd1c2af6eead0"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
   availability_zone = "us-east-1b"
   subnet_id = aws_subnet.private2.id
